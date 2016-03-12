91e1463de03ccc9e1d4222ed8ff436d1c9ce9efc
Mattias <mw222rs@student.lnu.se>
2016-03-12 22:19:20 +0100

    crawl sass-tweaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js'
-    },
-    module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader'
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loaders: [ 'style', 'css?sourceMap', 'autoprefixer', 'sass?sourceMap' ]
-            }
-        ]
-    },
-    plugins: []
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ["style", "css?sourceMap", "autoprefixer", "sass?sourceMap"]
+      }
+    ]
+  },
+  plugins: []
 };
