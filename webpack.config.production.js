2c5cbd7499e735bb2fd3b6092a23f7589a898c3b
Mattias <mw222rs@student.lnu.se>
2016-03-13 20:37:31 +0100

    changing to minified JS and separate minified css

---
+++ webpack.config.production.js
@@ -0,0 +1,37 @@
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!sass-loader")
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("assets/styles.css"),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })]
+};
